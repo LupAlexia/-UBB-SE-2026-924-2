ca6a5ac6c055b5632b27abf77df6e769fbdd4221
alexandrajol <alexandra.joldea@stud.ubbcluj.ro>
2026-05-07 14:09:35 +0300

    fix: unit tests services implementation stylecop

--- AirportApp/AirportApp.Tests.Unit/Services/implementation/FAQServiceTests.cs
+++ AirportApp/AirportApp.Tests.Unit/Services/implementation/FAQServiceTests.cs
@@ -1,25 +1,25 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using AirportApp.Src.Service.Implementation;
 using AirportApp.ClassLibrary.Repository.Interfaces;
 using AirportApp.ClassLibrary.Entity.Domain.Faq;
 using NSubstitute;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace AirportApp.Tests.Unit.Src.Service
 {
     [TestClass]
     public class FAQServiceTests
     {
-        private IFAQRepository _faqRepositoryMock;
-        private FAQService _faqService;
+        private IFAQRepository faqRepositoryMock;
+        private FAQService faqService;
 
         [TestInitialize]
         public void Setup()
         {
-            _faqRepositoryMock = Substitute.For<IFAQRepository>();
-            _faqService = new FAQService(_faqRepositoryMock);
+            faqRepositoryMock = Substitute.For<IFAQRepository>();
+            faqService = new FAQService(faqRepositoryMock);
         }
 
         [TestMethod]
@@ -31,13 +31,13 @@ namespace AirportApp.Tests.Unit.Src.Service
                 new FAQEntry(2, "Q2", "A2", FAQCategoryEnum.CheckIn, 0, 0, 0),
                 new FAQEntry(3, "Q3", "A3", FAQCategoryEnum.Baggage, 0, 0, 0)
             };
-            _faqRepositoryMock.GetByCategoryAsync(FAQCategoryEnum.CheckIn).Returns(Task.FromResult(faqs.Where(f => f.Category == FAQCategoryEnum.CheckIn).ToList()));
+            faqRepositoryMock.GetByCategoryAsync(FAQCategoryEnum.CheckIn).Returns(Task.FromResult(faqs.Where(f => f.Category == FAQCategoryEnum.CheckIn).ToList()));
 
-            var result = (await _faqService.GetByCategoryAsync(FAQCategoryEnum.CheckIn)).ToList();
+            var result = (await faqService.GetByCategoryAsync(FAQCategoryEnum.CheckIn)).ToList();
 
             Assert.AreEqual(2, result.Count);
             Assert.IsTrue(result.All(f => f.Category == FAQCategoryEnum.CheckIn));
-            await _faqRepositoryMock.Received(1).GetByCategoryAsync(FAQCategoryEnum.CheckIn);
+            await faqRepositoryMock.Received(1).GetByCategoryAsync(FAQCategoryEnum.CheckIn);
         }
 
         [TestMethod]
@@ -48,20 +48,20 @@ namespace AirportApp.Tests.Unit.Src.Service
                 new FAQEntry(1, "Q1", "A1", FAQCategoryEnum.CheckIn, 0, 0, 0),
                 new FAQEntry(2, "Q2", "A2", FAQCategoryEnum.Baggage, 0, 0, 0)
             };
-            _faqRepositoryMock.GetAllAsync().Returns(Task.FromResult((IEnumerable<FAQEntry>)faqs));
+            faqRepositoryMock.GetAllAsync().Returns(Task.FromResult((IEnumerable<FAQEntry>)faqs));
 
-            var result = (await _faqService.GetAllAsync()).ToList();
+            var result = (await faqService.GetAllAsync()).ToList();
 
             Assert.AreEqual(2, result.Count);
-            await _faqRepositoryMock.Received(1).GetAllAsync();
+            await faqRepositoryMock.Received(1).GetAllAsync();
         }
 
         [TestMethod]
         public async Task AddFAQEntry_WhenCalled_CallsRepository()
         {
             var faq = new FAQEntry(1, "Q1", "A1", FAQCategoryEnum.CheckIn, 0, 0, 0);
-            await _faqService.AddFAQEntryAsync(faq);
-            await _faqRepositoryMock.Received(1).CreateNewEntityAsync(faq);
+            await faqService.AddFAQEntryAsync(faq);
+            await faqRepositoryMock.Received(1).CreateNewEntityAsync(faq);
         }
     }
 }
