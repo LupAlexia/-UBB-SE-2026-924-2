1b3594798a7533cdb823b9b3d0768d7034c32cf8
AmaliaHaprian <amalia.haprian@stud.ubbcluj.ro>
2026-05-07 16:15:11 +0300

    create mvc project

---
+++ AirportApp/AirportAppGUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AirportAppGUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
