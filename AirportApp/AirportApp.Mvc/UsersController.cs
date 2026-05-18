8729460fa7190818ed858fd72b58f972a4a210d2
AmaliaHaprian <amalia.haprian@stud.ubbcluj.ro>
2026-05-18 21:36:58 +0300

    generate first controller

--- AirportApp/AirportApp.Mvc/UsersController.cs
+++ AirportApp/AirportApp.Mvc/UsersController.cs
@@ -12,17 +12,17 @@ namespace AirportApp.Mvc
 {
     public class UsersController : Controller
     {
-        private readonly AirportDbContext _context;
+        private readonly AirportDbContext context;
 
         public UsersController(AirportDbContext context)
         {
-            _context = context;
+            this.context = context;
         }
 
         // GET: Users
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Users.ToListAsync());
+            return View(await context.Users.ToListAsync());
         }
 
         // GET: Users/Details/5
@@ -33,7 +33,7 @@ namespace AirportApp.Mvc
                 return NotFound();
             }
 
-            var user = await _context.Users
+            var user = await context.Users
                 .FirstOrDefaultAsync(m => m.Id == id);
             if (user == null)
             {
@@ -58,8 +58,8 @@ namespace AirportApp.Mvc
         {
             if (ModelState.IsValid)
             {
-                _context.Add(user);
-                await _context.SaveChangesAsync();
+                context.Add(user);
+                await context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
             return View(user);
@@ -73,7 +73,7 @@ namespace AirportApp.Mvc
                 return NotFound();
             }
 
-            var user = await _context.Users.FindAsync(id);
+            var user = await context.Users.FindAsync(id);
             if (user == null)
             {
                 return NotFound();
@@ -97,8 +97,8 @@ namespace AirportApp.Mvc
             {
                 try
                 {
-                    _context.Update(user);
-                    await _context.SaveChangesAsync();
+                    context.Update(user);
+                    await context.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
                 {
@@ -124,7 +124,7 @@ namespace AirportApp.Mvc
                 return NotFound();
             }
 
-            var user = await _context.Users
+            var user = await context.Users
                 .FirstOrDefaultAsync(m => m.Id == id);
             if (user == null)
             {
@@ -139,19 +139,19 @@ namespace AirportApp.Mvc
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var user = await _context.Users.FindAsync(id);
+            var user = await context.Users.FindAsync(id);
             if (user != null)
             {
-                _context.Users.Remove(user);
+                context.Users.Remove(user);
             }
 
-            await _context.SaveChangesAsync();
+            await context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
         private bool UserExists(int id)
         {
-            return _context.Users.Any(e => e.Id == id);
+            return context.Users.Any(e => e.Id == id);
         }
     }
 }
